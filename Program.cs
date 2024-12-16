3455561b3435fa368841d9cac66f6c3a0df3dfdd
Сергей Неволин <sergeinevolin9@gmail.com>
2024-12-16 19:17:33 +0500

    вынес генерацию воркеров и тасок

--- Program.cs
+++ Program.cs
@@ -9,21 +9,10 @@ class Program
     static async Task Main(string[] args)
     {
         Console.WriteLine("Введите количество исполнителей:");
-        _ = int.TryParse(Console.ReadLine(), out int workerCount);
+        _ = int.TryParse(Console.ReadLine(), out int workersCount);
 
-        var workers = new List<IWorker>();
-        for (int i = 0; i < workerCount; i++)
-        {
-            workers.Add(new Worker($"Worker-{i + 1}", maxLoad: 30, maxTasks: 10));
-        }
-
-        var taskQueue = new Queue<TaskItem>();
-        var random = new Random();
-
-        for (int i = 0; i < 100; i++)
-        {
-            taskQueue.Enqueue(new TaskItem(random.Next(1, 6), random.Next(2, 10)));
-        }
+        var workers = GenerateWorkers(workersCount);
+        var taskQueue = GenerateTasks();
 
         var balancer = new Balancer(workers, taskQueue);
         var consoleMonitor = new ConsoleMonitor(balancer);
@@ -39,6 +28,10 @@ class Program
 
             await monitorTask;
         }
+        catch (TaskCanceledException)
+        {
+            Console.WriteLine("Задача отменена.");
+        }
         catch (Exception ex)
         {
             Console.WriteLine($"Произошла ошибка: {ex.Message}");
@@ -47,5 +40,29 @@ class Program
         {
             Console.WriteLine("Балансировщик завершил работу.");
         }
+    }
+
+    static private List<IWorker> GenerateWorkers(int workersCount = 3, int maxLoad = 30, int maxTasks = 10)
+    {
+        var workers = new List<IWorker>();
+        for (int i = 0; i < workersCount; i++)
+        {
+            workers.Add(new Worker($"Worker-{i + 1}", maxLoad, maxTasks));
+        }
+
+        return workers;
+    }
+
+    static private Queue<TaskItem> GenerateTasks(int tasksCount = 100)
+    {
+        var taskQueue = new Queue<TaskItem>();
+        var random = new Random();
+
+        for (int i = 0; i < tasksCount; i++)
+        {
+            taskQueue.Enqueue(new TaskItem(random.Next(1, 6), random.Next(2, 10)));
+        }
+
+        return taskQueue;
     }
 }
